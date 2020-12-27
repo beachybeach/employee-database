a86d3a76f94fb03f7262ad58608e133a2aa0ae7b
Riley <rsbeach97@gmail.com>
2020-12-27 09:57:14 -0700

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,18 @@ CREATE TABLE races (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30)
 );
+
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL NOT NULL,
+  group_id INT
+);
+
+CREATE TABLE employee (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT,
+  manager_id INT
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,14 @@
 INSERT INTO races (name)
 VALUES
-('Hobbits'),
-('Elves'),
-('Dwarves'),
-('Wizards');
+('Hobbits'), //farmer
+('Elves'), //archer
+('Dwarves'), //blacksmith
+('Wizards');//spellcaster
+
+INSERT INTO role (title, salary, group_id) 
+VALUES 
+('Farmer', 60000, 4),
+('Archer', 80000, 2),
+('Blacksmith', 70000, 3),
+('Spellcaster', 150000, 1);
+
