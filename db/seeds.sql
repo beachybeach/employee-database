5cb726dcd63e362e048ffd512392e03070296407
Riley <rsbeach97@gmail.com>
2020-12-27 11:27:36 -0700

    added employees list to employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,10 @@
 INSERT INTO races (name)
 VALUES
-('Hobbits'), //farmer
-('Elves'), //archer
-('Dwarves'), //blacksmith
-('Wizards');//spellcaster
+('Wizards'),
+('Elves'), 
+('Dwarves'), 
+('Hobbits'); 
+
 
 INSERT INTO role (title, salary, group_id) 
 VALUES 
@@ -11,4 +12,16 @@ VALUES
 ('Archer', 80000, 2),
 ('Blacksmith', 70000, 3),
 ('Spellcaster', 150000, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Legolas', 'GreenLeaf', 2, 1),
+('Gandalf', 'The Grey', 1, null),
+('Frodo', 'Baggins', 4, 1),
+('Samwise', 'Gamgee', 4, 1),
+('Gimli', 'Son of Gloin', 3, 1),
+('Thorin', 'Oakenshield', 3, 1),
+('Saruman', 'the White', 1, null);
+
+
 
